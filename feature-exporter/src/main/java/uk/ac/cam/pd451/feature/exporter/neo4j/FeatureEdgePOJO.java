7b7ee5e91e4ceee4669f2aeb73cce668f2756064
padr31 <pavol.drotar3@gmail.com>
2019-11-10 20:43:17 +0000

    Add gitignore and clean up untracked files

---
+++ feature-exporter/src/main/java/uk/ac/cam/pd451/feature/exporter/neo4j/FeatureEdgePOJO.java
@@ -0,0 +1,49 @@
+package uk.ac.cam.pd451.feature.exporter.neo4j;
+
+import org.neo4j.ogm.annotation.*;
+import uk.ac.cam.acr31.features.javac.proto.GraphProtos;
+
+import java.util.Map;
+
+@RelationshipEntity(type="FEATURE_EDGE")
+public class FeatureEdgePOJO {
+
+    public FeatureEdgePOJO() {}
+
+    public FeatureEdgePOJO(long sourceId, long destinationId, String edgeType, FeatureNodePOJO sourceNode, FeatureNodePOJO destinationNode) {
+        this.sourceId = sourceId;
+        this.destinationId = destinationId;
+        this.edgeType = edgeType;
+        this.sourceNode = sourceNode;
+        this.destinationNode = destinationNode;
+    }
+
+    public FeatureEdgePOJO(GraphProtos.FeatureEdge featureEdge,  Map<Long, FeatureNodePOJO> featureNodePOJOs) {
+        this(
+            featureEdge.getSourceId(),
+            featureEdge.getDestinationId(),
+            featureEdge.getType().toString(),
+            featureNodePOJOs.get(featureEdge.getSourceId()),
+            featureNodePOJOs.get(featureEdge.getDestinationId())
+        );
+    }
+
+    @Property
+    private long sourceId;
+
+    @Property
+    private long destinationId;
+
+    @Property
+    private String edgeType;
+
+    @Id @GeneratedValue
+    // need to use Long for generated IDs, long for inherited
+    private Long edgeId;
+
+    @StartNode
+    private FeatureNodePOJO sourceNode;
+
+    @EndNode
+    private FeatureNodePOJO destinationNode;
+}
